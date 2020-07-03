cdbb7ac151ed6f205e999e60e2b1141160f43814
Sunny Lee <leesunny507@gmail.com>
2020-07-03 17:38:34 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className='App'>
+        {/* content goes here */}
+      </main>
+    );
+  };
+};
+
+export default App;
