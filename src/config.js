cdd774c1f93b143577e7250c2f8bf349ccca477f
Sunny Lee <leesunny507@gmail.com>
2020-07-31 14:42:28 -0400

    deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  APIENDPOINT: /* process.env.REACT_APP_API_BASE_URL  ||  */'http://localhost:8000/api',
+  APIENDPOINT: process.env.REACT_APP_API_BASE_URL  || 'http://localhost:8000/api',
   TOKEN_KEY: 'cachow-client-auth-token',
 }
